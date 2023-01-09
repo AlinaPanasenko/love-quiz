95eae845e0b01d2721cbeb621dfa8c4616964e2a
AlinaPanasenko <maikshev@gmail.com>
2023-01-09 15:59:57 +0000

    Add preliminary html and css code. Start js code with runGame function

---
+++ assets/js/library.js
@@ -0,0 +1,20 @@
+
+export let questions = [
+{
+    question: "What is the nearest planet to the sun?",
+    options: ["Venus", "Mercury", "Saturn", "Uranus"],
+    answer: 1
+},
+{
+    question: "Name the person who won two separate Nobel prizes in two scientific fields.",
+    options: ["Marie Curie", "Linus Pauling", "Lord Rayleigh", "Uranus"],
+    answer: 0
+}
+
+
+
+
+
+
+
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
-console.log('Connected!');
+//Part of the code adapted from James Q Quick tutorial video. See Readme file for full credit.
+// ... helped me to connect js files together
+import { questions } from './library.js'
+
+const question = document.getElementById('question');
+const options = Array.from(document.getElementsByClassName('answer-option'));
+let questionIndex = 0;
+const MAX_Questions = 5;
+
+let currentQuestion = {};
+let acceptAnswer = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+// start and running the game and generating questions for quiz
+function runGame() {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = questions;
+    console.log(availableQuestions);
+    startNewQuiz();
+}
+
+function startNewQuiz() {
+
+    questionCounter++;
+    let questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+    
+}
+
+runGame();
