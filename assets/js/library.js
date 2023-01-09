5abb474757e26277d859e4d775771a14f1a8da14
AlinaPanasenko <maikshev@gmail.com>
2023-01-09 22:46:13 +0000

    Add 2 question to library file

--- assets/js/library.js
+++ assets/js/library.js
@@ -9,7 +9,18 @@ export let questions = [
     question: "Name the person who won two separate Nobel prizes in two scientific fields.",
     options: ["Marie Curie", "Linus Pauling", "Lord Rayleigh", "Uranus"],
     answer: 0
-}
+},
+{
+    question: "What was the most common name for Henry VIII's wives?",
+    options: ["Maria", "Anne", "Catherine", "Jane"],
+    answer: 2
+},
+{
+    question: "The logo for luxury car maker Porsche features which animal?",
+    options: ["Cheetah", "Lion", "Deer", "Horse"],
+    answer: 3
+},
+
 
 
 
