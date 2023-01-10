8ef2ffc9743da9e4e7609a0bf21b7876f128ff3a
AlinaPanasenko <maikshev@gmail.com>
2023-01-10 14:18:06 +0000

    Add js code for answer options

--- assets/js/library.js
+++ assets/js/library.js
@@ -2,13 +2,19 @@
 export let questions = [
 {
     question: "What is the nearest planet to the sun?",
-    options: ["Venus", "Mercury", "Saturn", "Uranus"],
-    answer: 1
+    option1: "Venus", 
+    option2: "Mercury",
+    option3: "Saturn", 
+    option4: "Uranus",
+    answer: 2
 },
 {
     question: "Name the person who won two separate Nobel prizes in two scientific fields.",
-    options: ["Marie Curie", "Linus Pauling", "Lord Rayleigh", "Uranus"],
-    answer: 0
+    option1: "Marie Curie",
+    option2: "Linus Pauling",
+    option3: "Lord Rayleigh",
+    option4: "Albert Einstein",
+    answer: 1
 },
 {
     question: "What was the most common name for Henry VIII's wives?",
@@ -16,12 +22,30 @@ export let questions = [
     answer: 2
 },
 {
-    question: "The logo for luxury car maker Porsche features which animal?",
+    question: "Which animal features the logo for the luxury car maker Porsche?",
     options: ["Cheetah", "Lion", "Deer", "Horse"],
     answer: 3
 },
-
-
+{
+    question: "What was the first chemically discovered element of periodic table?",
+    options: ["Phosphorus", "Copper", "Gold", "Carbon"],
+    answer: 0
+},
+{
+    question: "What was the first chemically discovered element of periodic table?",
+    options: ["Phosphorus", "Copper", "Gold", "Carbon"],
+    answer: 0
+},
+{
+    question: "What animal has a brain that is actually bigger than its eyes?",
+    options: ["Ostrich", "Cat", "Tarsier", "Chameleon"],
+    answer: 1
+},
+{
+    question: "Who discovered penicillin?",
+    options: ["Sir Henry Harris", "Joseph Lister", "Sir Alexander Fleming", "Frederick Banting"],
+    answer: 2
+},
 
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ import { questions } from './library.js'
 const question = document.getElementById('question');
 const options = Array.from(document.getElementsByClassName('answer-option'));
 let questionIndex = 0;
-const MAX_Questions = 5;
+const MAX_QUESTIONS = 5;
 
 let currentQuestion = {};
 let acceptAnswer = true;
@@ -19,15 +19,24 @@ function runGame() {
     score = 0;
     availableQuestions = questions;
     console.log(availableQuestions);
-    startNewQuiz();
+    getNextQuestion();
 }
 
-function startNewQuiz() {
+
+/*displays random question from the library and answer options for each question and removes
+from array question that have been asked */
+function getNextQuestion() {
 
     questionCounter++;
     let questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
+
+    options.forEach(function(option) {
+          const number = option.dataset['number'];
+          option.innerText = currentQuestion["option" + number];
+          console.log(number);
+    });
     
 }
 
